63b6149eca31e328588bb90827bc1a1432784a3e
Diyor <diyor.islomov@gmail.com>
2015-07-26 22:22:38 +0100

    Login activity init

---
+++ app/src/main/java/com/vodiytechnologies/rtcmsclient/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.vodiytechnologies.rtcmsclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Diyor on 7/26/2015.
+ */
+public class LoginActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
